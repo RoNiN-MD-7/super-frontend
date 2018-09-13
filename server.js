3b2f7bc21991cac1ad601dae68fc50b393e638c1
RoNiN-MD-7 <ronin.md.7@gmail.com>
2018-09-13 11:17:59 +0300

    Initial

---
+++ server.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer(function(request, response) {
+  console.log(request.method, request.url);
+  if (request.url == '/favicon.ico') {
+    const ico = fs.readFileSync('favicon.ico')
+    response.end(ico);
+  } else {
+    if (request.url == '/style.css') {
+      const css = fs.readFileSync('style.css', 'utf-8')
+      response.end(css);
+    } else {
+      const html = fs.readFileSync('index.html', 'utf-8');
+      response.end(html);
+    }
+  }
+});
+
+server.listen(3000);
+console.log("Server started!");
