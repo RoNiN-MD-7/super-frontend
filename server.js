8fa52e5c0073630bd887e245db0293d8b7a7bc88
RoNiN-MD-7 <ronin.md.7@gmail.com>
2018-09-13 12:19:34 +0300

    Add PORT

--- server.js
+++ server.js
@@ -19,5 +19,7 @@ const server = http.createServer(function(request, response) {
   }
 });
 
-server.listen(3000);
+console.log('port = ', process.env.PORT);
+
+server.listen(process.env.PORT || 3000);
 console.log("Server started!");
